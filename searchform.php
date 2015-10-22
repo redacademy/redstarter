ae001d7c34140baa76d09e2f367db0221ffb02a1
Mandi Wise <hello@mandiwise.com>
2015-10-22 15:30:51 -0700

    Add searchform.php template.

---
+++ searchform.php
@@ -0,0 +1,13 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<fieldset>
+		<label>
+			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+		</label>
+		<button class="search-submit">
+			<span class="icon-search" aria-hidden="true">
+				<i class="fa fa-search"></i>
+			</span>
+			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
+		</button>
+	</fieldset>
+</form>
