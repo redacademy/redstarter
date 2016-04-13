b0c973652943ffd3caec92a798f8f369f61e0fa7
Mandi Wise <hello@mandiwise.com>
2016-04-12 19:05:48 -0700

    Fix escaping error on comment template.

--- comments.php
+++ comments.php
@@ -67,7 +67,7 @@ if ( post_password_required() ) {
 
 	<?php comment_form( array(
 		'title_reply'          => esc_html( 'Post a Comment' ),
-		'comment_notes_before' => esc_html( '<p class="comment-notes">Want to join the discussion? Feel free to contribute!</p>' ),
+		'comment_notes_before' => wp_kses( '<p class="comment-notes">Want to join the discussion? Feel free to contribute!</p>', array( 'p' => array( 'class' => '' ) ) ),
 		'label_submit'         => esc_html( 'Submit' ),
 		'cancel_reply_link'    => esc_html( '[Cancel reply]' )
 	) ); ?>
