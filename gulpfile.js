32648b42359dd028d041ed69bf51ce073a5462c2
Mandi Wise <hello@mandiwise.com>
2018-01-31 18:13:00 -0800

    Update Gulp set-up.

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,74 @@
-var gulp = require('gulp'),
-    prettyError = require('gulp-prettyerror'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename = require('gulp-rename'),
-    cssnano = require('gulp-cssnano'),
-    uglify = require('gulp-uglify'),
-    eslint = require('gulp-eslint'),
-    browserSync = require('browser-sync');
+const gulp = require('gulp');
+const prettyError = require('gulp-prettyerror');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const eslint = require('gulp-eslint');
+const browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
-    gulp.src('./sass/style.scss')
-        .pipe(prettyError())
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./'))
-        .pipe(cssnano())
-        .pipe(rename('style.min.css'))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./build/css'));
+// Create basic Gulp tasks
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/style.scss', { sourcemaps: true })
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest('./'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('scripts', ['lint'], function () {
-    gulp.src('./js/*.js')
-        .pipe(uglify())
-        .pipe(rename({
-            extname: '.min.js'
-        }))
-        .pipe(gulp.dest('./build/js'))
+gulp.task('lint', function() {
+  return gulp
+    .src(['./js/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('lint', function () {
-    return gulp.src(['./js/*.js'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+gulp.task(
+  'scripts',
+  gulp.series('lint', function() {
+    return gulp
+      .src('./js/*.js')
+      .pipe(uglify())
+      .pipe(
+        rename({
+          extname: '.min.js'
+        })
+      )
+      .pipe(gulp.dest('./build/js'));
+  })
+);
+
+// Set-up BrowserSync and watch
+
+gulp.task('browser-sync', function() {
+  const files = [
+    './build/css/*.css',
+    './build/js/*.js',
+    './*.php',
+    './**/*.php'
+  ];
+
+  browserSync.init(files, {
+    proxy: 'localhost[:port-here]/[your-dir-name-here]'
+  });
+
+  gulp.watch(files).on('change', browserSync.reload);
 });
 
-gulp.task('browser-sync', function () {
-    var files = [
-        './build/css/*.css',
-        './build/js/*.js',
-        './*.php',
-        './**/*.php',
-    ];
-
-    browserSync.init(files, {
-        proxy: 'localhost[:port-here]/[your-dir-name-here]',
-    });
-
-    gulp.watch(files).on('change', browserSync.reload);
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', gulp.series('scripts'));
+  gulp.watch('sass/*.scss', gulp.series('sass'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./sass/*.scss', ['sass']);
-    gulp.watch('./js/*.js', ['scripts']);
-});
-
-gulp.task('default', ['watch', 'browser-sync']);
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
--- js/skip-link-focus-fix.js
+++ js/skip-link-focus-fix.js
@@ -5,29 +5,37 @@
  *
  * Learn more: https://github.com/Automattic/_s/pull/136
  */
-(function () {
-	var isWebkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
-		isOpera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
-		isIE = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
+(function() {
+  var isWebkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
+    isOpera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
+    isIE = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
 
-	if ((isWebkit || isOpera || isIE) && document.getElementById && window.addEventListener) {
-		window.addEventListener('hashchange', function () {
-			var id = location.hash.substring(1),
-				element;
+  if (
+    (isWebkit || isOpera || isIE) &&
+    document.getElementById &&
+    window.addEventListener
+  ) {
+    window.addEventListener(
+      'hashchange',
+      function() {
+        var id = location.hash.substring(1),
+          element;
 
-			if (!(/^[A-z0-9_-]+$/.test(id))) {
-				return;
-			}
+        if (!/^[A-z0-9_-]+$/.test(id)) {
+          return;
+        }
 
-			element = document.getElementById(id);
+        element = document.getElementById(id);
 
-			if (element) {
-				if (!(/^(?:a|select|input|button|textarea)$/i.test(element.tagName))) {
-					element.tabIndex = -1;
-				}
+        if (element) {
+          if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+            element.tabIndex = -1;
+          }
 
-				element.focus();
-			}
-		}, false);
-	}
+          element.focus();
+        }
+      },
+      false
+    );
+  }
 })();
